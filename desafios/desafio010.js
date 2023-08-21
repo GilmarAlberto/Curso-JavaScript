21f35f9a0f850e5729a2b057990452e9f4aef874
Gilmar Alberto <gilabreu00@gmail.com>
2023-08-21 12:33:51 -0300

    desafio chatgpt

---
+++ desafios/desafio010.js
@@ -0,0 +1,10 @@
+/*
+Exercício 1: Contagem Crescente Escreva um programa que use um loop while para imprimir os números de 1 a 10, um em cada linha.
+*/
+let num = 1;
+
+while (num <=10){
+    console.log(num);
+
+    num++;
+}
